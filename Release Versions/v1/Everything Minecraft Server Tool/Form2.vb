6197869cc54d8bc2d4fcd31925fb8d89d85ca8a4
Jonathan Barrett <jblackops99@gmail.com>
2016-05-30 18:45:39 -0600

    Version 1
    Version 1 has just been rreleased hope it works for everyone and
    everyone likes it.

---
+++ Release Versions/v1/Everything Minecraft Server Tool/Form2.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(1)
+
+        If ProgressBar1.Value = 100 Then
+            ProgressBar1.Value = 0
+        End If
+        Label1.Text = "Checking Settings"
+        Timer1.Enabled = False
+        Form1.Show()
+    End Sub
+End Class
